410a3856d9b61e1990695ebb1b6b06ac0eac6e18
Arthur Costa da Silva <arthur.silva@pampa.compasso>
2020-01-01 03:09:00 -0300

    feat: adjusted flow on chat

--- server/src/app.js
+++ server/src/app.js
@@ -1,72 +1,71 @@
-import cors from 'cors'
-import express from 'express'
-import bodyParser from 'body-parser'
+import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
 
-import Response from './shared/utils/response'
-import routes from './routes'
+import Response from "./shared/utils/response";
+import routes from "./routes";
 
 class App extends Response {
   constructor() {
-    super()
-    this.express = express()
-    this.middlewares()
-    this.routes()
+    super();
+    this.express = express();
+    this.middlewares();
+    this.routes();
   }
 
   middlewares() {
-    this.express.use(express.json())
-    this.express.use(bodyParser.urlencoded({ extended: true }))
-    this.express.use(bodyParser.json({ limit: '2mb ' }))
+    this.express.use(express.json());
+    this.express.use(bodyParser.urlencoded({ extended: true }));
+    this.express.use(bodyParser.json({ limit: "2mb " }));
     this.express.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*')
+      res.header("Access-Control-Allow-Origin", "*");
       res.header(
-        'Access-Control-Allow-Headers',
-        'X-Requested-With, Content-Type, Authorization'
-      )
+        "Access-Control-Allow-Headers",
+        "X-Requested-With, Content-Type, Authorization"
+      );
       res.header(
-        'Access-Control-Allow-Methods',
-        'GET,PUT,PATCH,POST,DELETE,OPTIONS'
-      )
-      next()
-    })
-    this.express.use(cors())
-    this.express.use(Jwt.authorization())
+        "Access-Control-Allow-Methods",
+        "GET,PUT,PATCH,POST,DELETE,OPTIONS"
+      );
+      next();
+    });
+    this.express.use(cors());
   }
 
   routes() {
-    this.express.get('/health', (req, res) => {
-      res.status(200).json({ server: "server it's works!" })
-    })
+    this.express.get("/health", (req, res) => {
+      res.status(200).json({ server: "server it's works!" });
+    });
 
-    this.express.use(routes)
+    this.express.use(routes);
 
     this.express.use((req, res, next) => {
       const response = {
         status: 404,
-        messages: this.messages.ROUTER_NOT_FOUND,
-      }
-      this.sendError(res, next, response)
-      next()
-    })
+        messages: this.messages.ROUTER_NOT_FOUND
+      };
+      this.sendError(res, next, response);
+      next();
+    });
 
     this.express.use((error, req, res, next) => {
-      if (error.name === 'UnauthorizedError') {
-        const response = { status: 401, messages: this.messages.TOKEN_INVALID }
-        this.sendError(res, next, response)
+      if (error.name === "UnauthorizedError") {
+        const response = { status: 401, messages: this.messages.TOKEN_INVALID };
+        this.sendError(res, next, response);
       }
-      next()
-    })
+      next();
+    });
 
     this.express.use((error, req, res, next) => {
       const response = {
         status: error.status || 500,
         messages: this.messages.TOKEN_INVALID,
-        error: error.message,
-      }
-      this.sendError(res, next, response)
-      next()
-    })
+        error: error.message
+      };
+      this.sendError(res, next, response);
+      next();
+    });
   }
 }
 
-export default new App().express
+export default new App().express;
--- server/src/server.js
+++ server/src/server.js
@@ -3,8 +3,65 @@ import http from 'http'
 
 import app from './app'
 import { env } from './config/app'
+import {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} from './shared/models/users'
 
 const server = http.createServer(app)
 const io = socketIO(server)
+
+io.on('connection', socket => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room })
+
+    if (error) return callback(error)
+
+    socket.join(user.room)
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}, welcome to the room ${user.room}`,
+    })
+
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: 'admin', text: `${user.name}, has joined!` })
+
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    })
+
+    callback()
+  })
+
+  socket.on('sendMessage', (message, callback) => {
+    const { user, error } = getUser(socket.id)
+
+    if (error) return callback(error)
+
+    io.to(user.room).emit('message', { user: user.name, text: message })
+
+    callback()
+  })
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id)
+
+    if (user) {
+      io.to(user.room).emit('message', {
+        user: 'admin',
+        text: `${user.name} has left.`,
+      })
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      })
+    }
+  })
+})
 
 server.listen(env.PORT, () => console.log(`Listening on port: ${env.PORT}`))
